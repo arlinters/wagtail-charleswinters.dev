30b263acbe810906ba215b38ff72c41b2200baad
Charles Love Winters III <charlie.winters17@gmail.com>
2022-04-10 15:58:08 -0500

    add config with django-heroku

--- project/settings/base.py
+++ project/settings/base.py
@@ -187,4 +187,5 @@ WAGTAILMARKDOWN = {
 }
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'http://example.com'import django_heroku
+django_heroku.settings(locals())
