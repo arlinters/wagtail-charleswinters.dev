fc7e07ba748c09782300e1e98a246a3f9cd04e46
Charles Love Winters III <charlie.winters17@gmail.com>
2022-06-04 09:02:26 -0500

    init commit of rebuild

---
+++ project/project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!w7dx--6k)7cq0skrlo43m-0d@l&_v)^1u0fz@k7nvt%d3$u1@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
