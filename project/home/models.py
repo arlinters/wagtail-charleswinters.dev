eb1b7f4acd09498a30ba6174757bc3e4fd147a30
Charles Love Winters III <charlie.winters17@gmail.com>
2022-06-04 19:01:04 -0500

    refactor docker files for easier dev

--- project/home/models.py
+++ project/home/models.py
@@ -1,7 +1,9 @@
+from dataclasses import Field
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    ...
